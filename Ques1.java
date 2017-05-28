cdd807dcf05b26997fa2021cdf655177970fab51
Alexabel <Alexabel219@gmail.com>
2017-05-28 14:33:24 -0700

    Update Ques1.java

--- Ques1.java
+++ Ques1.java
@@ -1,14 +1,18 @@
 package ques1; 
 import java.util.Scanner;
+//importing scanner(object)
 
 
 public class Ques1 {
     
       public static void main(String[] args) {
           System.out.println("Enter a number");
+          //telling user to enter a number
           Scanner x = new Scanner(System.in);
                   int no = x.nextInt(); 
+          //declaring where the int is to be stored
             if((no%2) == 0){System.out.println("this num is even");
+                            //making the if statement
             }
             else{System.out.println("this number is odd");}
       }
